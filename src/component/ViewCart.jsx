5fb495f8ae18c5b09be0bab847e52b6bb04517be
Abhay kant tiwari <tabhaykant2@gmail.com>
2025-09-26 22:27:37 +0530

    update view cart page

--- src/component/ViewCart.jsx
+++ src/component/ViewCart.jsx
@@ -41,11 +41,69 @@ const ViewCart = () => {
                             <button className="font-medium hover:text-primary cursor-pointer">REMOVE</button>
                         </div>
                     </div>
+                      <div className="mt-4 bg-white p-3 ">
+                        <div className="flex gap-2 justify-between pt-2">
+                            <div className="flex gap-2">
+                                <div>
+                                <img src="https://rukminim2.flixcart.com/image/224/224/l4zxn680/curtain/v/m/p/long-crush-plain-curtain-long-door-plain-curtain-12-eyelet-skj-original-imagfrgjrnpdsc8y.jpeg?q=90" className="w-[120px]" alt="" />
 
+                            </div>
+                            <div className="leading-8">
+                                <h3 className="text-[15px]">Galaxy Home Decor 182 cm (6 ft) Polyester Semi </h3>
+                                 <p className="text-gray-500 text-[13px]">Size: 8,Black , 8</p>
+                                <p className="text-gray-500 text-[13px]">Seller:GalaxyHomeDecorIndia</p>
+                               
+                                <div className="flex gap-2 text-gray-500"><span className="line-through text-gray-400">₹3969</span> <span className="font-[500]">₹1830</span><span className="text-green-700 font-[500]">54% off</span></div>
+                            </div>
+                            </div>
+                            <div className="leading-8">
+                                <h1 className="text-[15px]">Delivery by Thu Oct 9</h1>
+                            </div>
+                        </div>
+                        <div className="flex gap-5 mt-4">
+                            <div className="flex items-center gap-2">
+                                <button className="bg-gray-200 border-1 border-gray-300 hover:bg-gray-100 w-[30px] h-[30px] text-black flex items-center justify-center rounded-full cursor-pointer shadow-2xl">-</button>
+                                <input className="w-[50px] text-center border-1 border-gray-300 p-1" type="text" name="" value="1" id="" />
+                                <button className="bg-gray-200 border-1 border-gray-300 hover:bg-gray-100 w-[30px] h-[30px] text-black flex items-center justify-center rounded-full cursor-pointer shadow-2xl">+</button>
+                            </div>
+                            <button className="font-medium hover:text-primary cursor-pointer">REMOVE</button>
+                        </div>
+                    </div>
+                      <div className="mt-4 bg-white p-3 ">
+                        <div className="flex gap-2 justify-between pt-2">
+                            <div className="flex gap-2">
+                                <div>
+                                <img src="https://rukminim2.flixcart.com/image/224/224/l4zxn680/curtain/v/m/p/long-crush-plain-curtain-long-door-plain-curtain-12-eyelet-skj-original-imagfrgjrnpdsc8y.jpeg?q=90" className="w-[120px]" alt="" />
+
+                            </div>
+                            <div className="leading-8">
+                                <h3 className="text-[15px]">Galaxy Home Decor 182 cm (6 ft) Polyester Semi </h3>
+                                 <p className="text-gray-500 text-[13px]">Size: 8,Black , 8</p>
+                                <p className="text-gray-500 text-[13px]">Seller:GalaxyHomeDecorIndia</p>
+                               
+                                <div className="flex gap-2 text-gray-500"><span className="line-through text-gray-400">₹3969</span> <span className="font-[500]">₹1830</span><span className="text-green-700 font-[500]">54% off</span></div>
+                            </div>
+                            </div>
+                            <div className="leading-8">
+                                <h1 className="text-[15px]">Delivery by Thu Oct 9</h1>
+                            </div>
+                        </div>
+                        <div className="flex gap-5 mt-4">
+                            <div className="flex items-center gap-2">
+                                <button className="bg-gray-200 border-1 border-gray-300 hover:bg-gray-100 w-[30px] h-[30px] text-black flex items-center justify-center rounded-full cursor-pointer shadow-2xl">-</button>
+                                <input className="w-[50px] text-center border-1 border-gray-300 p-1" type="text" name="" value="1" id="" />
+                                <button className="bg-gray-200 border-1 border-gray-300 hover:bg-gray-100 w-[30px] h-[30px] text-black flex items-center justify-center rounded-full cursor-pointer shadow-2xl">+</button>
+                            </div>
+                            <button className="font-medium hover:text-primary cursor-pointer">REMOVE</button>
+                        </div>
+                    </div>
+                    <div className="placeOrder sticky bottom-0 bg-white p-5 flex justify-end">
+                        <button  className="bg-primary text-white font-medium px-7 py-3">PLACE ORDER</button>
+                    </div>
                 </div>
 
                 {/* Right Sidebar */}
-                <div className="w-[29%] mt-4 bg-white p-3 rounded-lg shadow">
+                <div className="w-[29%]  mt-4 bg-white p-3 rounded-lg shadow">
                     <h1 className="text-gray-500 font-semibold p-2">PRICE DETAILS</h1>
                     <Divider />
 
