3644307da0a22038782c6a89f710af9c834aa4da
Abhay kant tiwari <tabhaykant2@gmail.com>
2025-10-05 19:48:20 +0530

    make product section in dashboard

--- admin/src/pages/Dashboard.jsx
+++ admin/src/pages/Dashboard.jsx
@@ -6,6 +6,8 @@ import img1 from "../assets/1.png"
 import { FaChevronDown, FaPlus } from 'react-icons/fa'
 import useHandleAccordion from '../hooks/useHandleAccordion'
 import OrderStatus from '../component/OrderStatus'
+import shoes from "../assets/shoes.webp"
+import { Link } from 'react-router'
 
 const Dashboard = () => {
     const orders = [
@@ -69,6 +71,122 @@ const Dashboard = () => {
                         <Slide />
                     </div>
 
+                    {/* product */}
+
+
+                    <div className="card mx-3">
+                        <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                            <div className='p-2'>
+                                <h1 className='text-[18px] font-[500]'>Products</h1>
+                            </div>
+                            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th scope="col" class="p-4">
+                                            <div class="flex items-center">
+                                                <input id="checkbox-all-search" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                <label for="checkbox-all-search" class="sr-only">checkbox</label>
+                                            </div>
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Image
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Product name
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Category
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Sub Category
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Brand
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Price
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Rating
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Rating
+                                        </th>
+                                        <th scope="col" class="px-6 py-3">
+                                            Action
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
+                                        <td class="w-4 p-4">
+                                            <div class="flex items-center">
+                                                <input id="checkbox-table-search-1" type="checkbox" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                                                <label for="checkbox-table-search-1" class="sr-only">checkbox</label>
+                                            </div>
+                                        </td>
+                                        <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <img src={shoes} alt="" className='w-[70px]' />
+                                        </th>
+                                        <td class="px-6 py-4">
+                                            <Link to="/products/121" className='hover:text-red-500'>Luft Pace M Running Shoes For Men</Link>
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Shoes
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            ADIDAS 
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            ADIDAS 
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            1,836
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            Laptop
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            4
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                                        </td>
+                                    </tr>
+
+
+                                </tbody>
+                            </table>
+                            <nav class="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4" aria-label="Table navigation">
+                                <span class="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">Showing <span class="font-semibold text-gray-900 dark:text-white">1-10</span> of <span class="font-semibold text-gray-900 dark:text-white">1000</span></span>
+                                <ul class="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" aria-current="page" class="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">4</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">5</a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+
+
                     {/* Orders Section */}
                     <div className="card mx-3">
                         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
@@ -99,7 +217,7 @@ const Dashboard = () => {
 
                             {/* Orders Table */}
                             <div className="pb-4 bg-white dark:bg-gray-900 flex items-center justify-between">
-                                <div className="overflow-x-auto max-h-[500px] rounded-lg scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
+                                <div className=" max-h-[500px] rounded-lg scrollbar-thin scrollbar-thumb-gray-300 scrollbar-track-gray-100">
                                     <table className="min-w-full text-sm text-left border-collapse">
                                         {/* Table Head */}
                                         <thead className="bg-gray-100 text-gray-700 uppercase text-xs tracking-wider sticky top-0 z-10">
@@ -153,8 +271,8 @@ const Dashboard = () => {
 
                                                     {/* Accordion Row */}
                                                     <tr>
-                                                        <td></td>
-                                                        <td colSpan={11} className="p-0">
+
+                                                        <td colSpan={12} className="p-0">
                                                             <div
                                                                 className={`overflow-hidden transition-all duration-300 ease-in-out ${clickIndex === index ? "max-h-96 opacity-100 p-4" : "max-h-0 opacity-0 p-0"
                                                                     }`}
